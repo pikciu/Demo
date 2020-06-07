d84bc55e33d1920ffbe6358975fe013f20602a53
Tomasz Pikć <tomasz.pikc@apzumi.pl>
2020-06-07 03:25:10 +0200

    DEMO-0 add modules (app, data, domain), setup pods

---
+++ Domain/Domain/Domain.h
@@ -0,0 +1,19 @@
+//
+//  Domain.h
+//  Domain
+//
+//  Created by Tomasz Pikć on 07/06/2020.
+//  Copyright © 2020 pikciu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Domain.
+FOUNDATION_EXPORT double DomainVersionNumber;
+
+//! Project version string for Domain.
+FOUNDATION_EXPORT const unsigned char DomainVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Domain/PublicHeader.h>
+
+
